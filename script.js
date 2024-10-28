dad2d14067b712178bc55e97aead51851ebaa37e
Kenneth Kwan <kwan.k1203@csu.fullerton.edu>
2024-10-28 04:44:40 -0700

    Basic password hash

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
+const readline = require('readline') // Temp for testing inputs
 
 const User = require('./users')
 const Exhibit = require('./exhibits')
 
 mongoose.connect('mongodb://localhost:27017/ocexhibit')
 
-const user = new User({
-    name: 'John Doe',
-    age: 25,
-    email: 'email@gmail.com'
-})
-user.save()
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-const exhibit = new Exhibit({
-    name: 'Art Exhibit',
-    description: 'An exhibit of art',
-    address: '123 Main St.',
-    date: new Date(),
-    price: 10
-})
-exhibit.save()
+async function createUsers() {
+    rl.question('Enter your name: ', (name) => {
+        rl.question('Enter your email: ', (email) => {
+            rl.question('Enter your password: ', async (password) => {
+                const hash = await bcrypt.hash(password, 10)
+                const user = new User({
+                    name: name,
+                    email: email,
+                    password: hash
+                })
+                user.save()
+                console.log(user)
+
+                const isMatch = await bcrypt.compare("Password1", hash)
+                console.log(isMatch)
+
+                rl.close()
+            })
+        })
+    })
+};
+
+createUsers()
 
 
-console.log(user)
-console.log(exhibit)
+//const hash = await bcrypt.hashSync(password, 10)
+
+
+// console.log(user)
+// console.log(exhibit)
